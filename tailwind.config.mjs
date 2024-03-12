3f2fa3688135473a364040a285621feddf60ebcd
Erick Mireles <emireles.rosas@gmail.com>
2024-03-12 15:48:54 -0600

    Start over with a new design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,19 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'dark-1': 'hsl(0, 0%, 8%)',
+				'dark-1': 'hsl(240, 24%, 8%)',
 				'dark-2': 'hsl(42, 6%, 31%)',
-				'brand-1': 'hsl(40, 100%, 48%)',
-				'brand-2': 'hsl(42, 100%, 90%)',
-				'light-1': 'hsl(42, 100%, 98%)',
-				'light-2': 'hsl(48, 8%, 75%)'
+				'brand-1': 'hsl(240, 70%, 64%)',
+				'brand-2': 'hsl(240, 60%, 44%)',
+				'light-1': 'hsl(240, 50%, 98%)',
+				'light-2': 'hsl(240, 8%, 75%)'
 			},
 			fontFamily: {
-				display: ['Red Hat Display', 'sans-serif'],
-				body: ['Poppins', 'sans-serif']
+				display: ['Neue-Regrade-Variable', 'sans-serif'],
+				body: ['Satoshi-Variable', 'sans-serif']
+			},
+			spacing: {
+				page: 'var(--page-main)'
 			}
 		}
 	},

18f36b0cc17e7b5bed5ea3899118568704043edd
Erick Mireles <emireles.rosas@gmail.com>
2024-03-13 11:58:16 -0600

    Update Astro and dependencies to latest versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react()]
-});
+	integrations: [icon(), tailwind(), react()]
+})

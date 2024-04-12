b5b19920b26f27fed90255d4cf8265c711f2fea7
Erick Mireles <emireles.rosas@gmail.com>
2024-04-11 22:50:35 -0600

    Add tooltip to inactive Home page button

--- src/components/react/SwitcherArrows.jsx
+++ src/components/react/SwitcherArrows.jsx
@@ -1,8 +1,8 @@
 function SwitcherArrows({ onClickPrevious, onClickNext }) {
   return (
-      <div className='absolute px-4 bottom-[18px] flex items-center justify-between w-full gap-4'>
-        <button className="px-16 py-2 rounded-md bg-brand-1 hover:-translate-y-[2px] transition-all duration-200 shadow-sm hover:shadow-highlight" onClick={onClickPrevious}>←</button>
-        <button className="px-16 py-2 rounded-md bg-brand-1 hover:-translate-y-[2px] transition-all duration-200 shadow-sm hover:shadow-highlight" onClick={onClickNext}>→</button>
+      <div className='absolute right-4 bottom-[18px] flex items-center gap-4'>
+        <button className="px-4 py-2 rounded-md bg-brand-1 hover:-translate-y-[2px] transition-all duration-200 shadow-sm hover:shadow-highlight" onClick={onClickPrevious}>←</button>
+        <button className="px-4 py-2 rounded-md bg-brand-1 hover:-translate-y-[2px] transition-all duration-200 shadow-sm hover:shadow-highlight" onClick={onClickNext}>→</button>
       </div>
   );
 }

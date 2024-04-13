aaee75f7a68014f0051932e323df1d79b87eeddb
Erick Mireles <emireles.rosas@gmail.com>
2024-04-12 22:56:32 -0600

    Implement mouseover interaction for Card elements

--- src/lib/cardAnimation.js
+++ src/lib/cardAnimation.js
@@ -1,6 +1,6 @@
 import { gsap } from 'gsap'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('astro:page-load', () => {
   const cards = Array.from(document.querySelector('#cards-grid').children)
   cards.forEach((card) => {
     card.classList.add('opacity-0', 'translate-y-10')
@@ -27,4 +27,44 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     '-=0.3'
   )
+
+  const calculateRotation = (e, parent) => {
+    const cardRect = parent.getBoundingClientRect()
+    console.log(cardRect.width, cardRect.height)
+
+    const centerX = cardRect.left + cardRect.width / 2
+    const centerY = cardRect.top + cardRect.height / 2
+
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const offsetX = ((mouseX - centerX) / centerX) * 3
+    let offsetY = ((mouseY - centerY) / centerY) * 5
+    if (cardRect.height > 600) {
+      offsetY = ((mouseY - centerY) / centerY) * 1.5
+    }
+
+    gsap.to(parent, {
+      rotateX: offsetY,
+      rotateY: -offsetX,
+      ease: 'linear'
+    })
+  }
+
+  cards.forEach((card) => {
+    card.addEventListener('mousemove', (e) => {
+      calculateRotation(e, card)
+    })
+  })
+
+  cards.forEach((card) => {
+    card.addEventListener('mouseleave', () => {
+      gsap.to(card, {
+        duration: 0.5,
+        rotateX: 0,
+        rotateY: 0,
+        ease: 'power2.out'
+      })
+    })
+  })
 })

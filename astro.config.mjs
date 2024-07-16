ff70e0f4efba935646424c7cbce11004bedcbebd
Erick Mireles <emireles.rosas@gmail.com>
2024-07-16 13:28:36 -0400

    Remove Astro DB from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import icon from 'astro-icon';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react(), db()]
+  integrations: [icon(), tailwind(), react()]
 });
